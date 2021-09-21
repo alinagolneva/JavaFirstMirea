027ffe9c4b07bcb7ddacd3a5c90fc17b77d290f4
Alina <alinagrey830@gmail.com>
2021-09-21 18:20:39 +0300

    2prak

--- src/ru/mirea/task4/TestAuthor.java
+++ src/ru/mirea/task4/TestAuthor.java
@@ -5,5 +5,6 @@ public class TestAuthor
     public static void main(String[] args) {
         Author a1 = new Author("Ivanova", "ivanovaa@gmail.com", 'F');
         System.out.println(a1.toString());
+        a1.getEmail();
     }
 }
